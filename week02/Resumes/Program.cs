4b00aa55bbbe6b64274f9dafd13381cae13d7e8a
Luccas Aguilar <luccasaguilar@gmail.com>
2025-09-09 12:27:31 -0400

    week 02

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "LNA Systems";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2016;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2023;
+        job2._endYear = 2025;
+
+        Resume resume = new Resume();
+        resume._name = "Luccas Aguilar";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
+
     }
 }
