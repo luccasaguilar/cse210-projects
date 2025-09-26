05ac565d57266000e776fdeea6e60c8c7b86c1bd
Luccas Aguilar <luccasaguilar@gmail.com>
2025-09-26 18:42:54 -0400

    week 04

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,18 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        var addr1 = new Address("1200 Market St", "St. Louis", "MO", "USA");
-        var cust1 = new Customer("Alice Carter", addr1);
+        var addr1 = new Address("600 Plates Way", "Zarahemla", "UT", "USA");
+        var cust1 = new Customer("Captain Moroni", addr1);
         var order1 = new Order(cust1);
-        order1.AddProduct(new Product("Wireless Mouse", "WM-100", 19.99m, 2));
-        order1.AddProduct(new Product("USB-C Cable", "UC-10", 7.50m, 3));
-        order1.AddProduct(new Product("Laptop Stand", "LS-90", 32.00m, 1));
+        order1.AddProduct(new Product("Title of Liberty Banner", "TL-001", 24.99m, 2));
+        order1.AddProduct(new Product("Stripling Warrior Shield", "SW-200", 39.50m, 1));
+        order1.AddProduct(new Product("Liahona Replica", "LH-777", 59.00m, 1));
 
-        var addr2 = new Address("1234 Baker Street", "Toronto", "ON", "CANADA");
-        var cust2 = new Customer("Arthur Smith", addr2);
+        var addr2 = new Address("12 Valley of Lemuel Rd", "Bountiful", "Alma", "Guatemala");
+        var cust2 = new Customer("Nephi son of Lehi", addr2);
         var order2 = new Order(cust2);
-        order2.AddProduct(new Product("Noise-Canceling Headphones", "NH-500", 129.99m, 1));
-        order2.AddProduct(new Product("Portable SSD 1TB", "PS-1TB", 89.00m, 2));
+        order2.AddProduct(new Product("Brass Plates Copy", "BP-316", 79.99m, 1));
+        order2.AddProduct(new Product("Shipbuilding Tools Set", "ST-008", 129.00m, 1));
+        order2.AddProduct(new Product("Liahona Compass", "LC-101", 45.75m, 2));
 
         var orders = new List<Order> { order1, order2 };
 
