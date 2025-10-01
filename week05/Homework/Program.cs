da76fee67aaa6c1ddbc437dac50245c70d89097d
Luccas Aguilar <luccasaguilar@gmail.com>
2025-10-01 10:53:24 -0400

    week 05

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        var assignment1 = new Assignment("Luccas Aguilar", "Test");
+        Console.WriteLine(assignment1.GetSummary());
+
+        var assignment2 = new MathAssignment("Jose Aguilar", "Test 2", "1.2", "8-9");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        var assignment3 = new WritingAssignment("Maria Aguilar", "Test 3", "The Test of Assignment");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
