bc030cb6bcb49c7ae196af7707a7940fc1bf51e9
Luccas Aguilar <luccasaguilar@gmail.com>
2025-10-10 17:12:00 -0400

    comments

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,5 +1,6 @@
 using System;
-
+//I created a new menu item that allows you to list only the goal names.
+//I created the option to create negative goals, which subtract points instead of adding them.
 class Program
 {
     static void Main(string[] args)
